b9527ab795ccc7ad1db519d332e68750e7897586
Domonkos Suranyi <suranyi.domi@gmail.com>
2018-03-20 18:43:23 +0100

    Table module minimal definition added

--- src/table.rs
+++ src/table.rs
@@ -1,8 +1,38 @@
+pub struct Table {
+    fields: Vec<Vec<Field>>
+}
+
+#[derive(Clone, PartialEq, Debug)]
 pub enum Field {
     X,
     O,
     Empty,
 }
 
-pub type Table = Vec<Vec<Field>>;
+pub struct Pos {
+    x: u8,
+    y: u8,
+}
 
+impl Table {
+    pub fn new(size: Pos) -> Self {
+        Table { fields: vec![vec![Field::Empty; size.y as usize]; size.x as usize] }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Table;
+    use super::Field;
+    use super::Pos;
+
+    #[test]
+    fn new() {
+        let table = Table::new(Pos { x:10, y:5 });
+        for i in 0..10 {
+            for j in 0..5 {
+                assert_eq![table.fields[i][j], Field::Empty]
+            }
+        }
+    }
+}

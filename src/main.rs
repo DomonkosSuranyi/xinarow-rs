45bd99da698b1f4cb45ee4b0b93db08f502edae5
Domonkos Suranyi <suranyi.domi@gmail.com>
2018-05-31 06:36:06 +0200

    User can define the size of the table

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 pub mod table;
 use table::{Table};
+use std::result::Result;
 
-fn main() {
-    let table_size = (5,3);
-    let mut table = Table::new(table_size);
+type GenError = Box<std::error::Error>;
+type GenResult<T> = Result<T, GenError>;
+
+fn main() -> GenResult<()> {
+    println!("Width of the table: ");
+    let width = read_usize_input()?;
+    println!("height of the table: ");
+    let height = read_usize_input()?;
+    let mut table = Table::new((width, height));
     println!("{}", table);
+
+    Ok(())
+}
+
+fn read_usize_input() -> GenResult<usize> {
+    let mut value_in = String::new();
+    std::io::stdin().read_line(&mut value_in)?;
+    Ok(value_in.trim().parse::<usize>()?)
 }
 
